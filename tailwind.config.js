a9a75a5c2f472ecfb9bdfde4150c2b24e1e34e0f
Joe Torreggiani <joetorreggiani@gmail.com>
2022-12-23 23:34:22 -0500

    Initial commit.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tw-elements/dist/plugin'),
+  ],
+}
