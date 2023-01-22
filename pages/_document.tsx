d26b47a6b1cf5cdecaa67dc12c125a97e151260f
Joe Torreggiani <joetorreggiani@gmail.com>
2023-01-21 22:21:59 -0500

    Fix property issue.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head />
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap" rel="stylesheet"></link>
       <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+      <link rel="preconnect" href="https://fonts.gstatic.com"></link>
       <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text:ital@0;1&family=Inter:wght@100;200;300;400;500;600;700&family=Lato:ital,wght@0,100;0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"></link>
       <body>
         <Main />
