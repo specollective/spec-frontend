2d5318b18afe969ddee765e47c5d0d0b418dbc54
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-04-07 15:47:05 -0700

    edited styling for scale

--- pages/Hero.tsx
+++ pages/Hero.tsx
@@ -8,23 +8,26 @@ export default function Hero() {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
   return (
-    <section className="pb-29 flex pl-32">
-      <div id="top" className="pr-[70px]">
-        <h1 className="mb-11 w-[35rem] pt-20 font-dmserif text-8xl">
+    <section className="pb-29 flex max-w-[90rem] pl-[8rem]">
+      <div id="top" className="pr-[8rem]">
+        {/* change width depending on scaling max-w[35rem]*/}
+        <h1 className="mb-11 w-[35rem] pt-20 pb-[3rem] font-dmserif text-8xl">
           Paid training to promote equity and progress
         </h1>
-        <p className="w-96 font-montserrat text-3xl font-medium leading-10 text-spec-turquiose">
+        {/* scaling max-w-[25rem] */}
+        <p className=" w-[25rem] font-montserrat text-3xl font-medium leading-10 text-spec-turquiose">
           SPEC provides paid learning opportunities by partnering with clients
           to build websites, hardware, art, and written content.
         </p>
       </div>
-      <div className="relative min-w-[743px]">
-        <Image src={training} alt="training-photo" />
+      {/* scaling min-h-[55rem] max-w-[45rem] */}
+      <div className="relative">
+        <Image src={training} alt="training-photo" className="min-w-[45rem]" />
         <button onClick={toTop}>
           <Image
             src={topButton}
             alt="back-to-top"
-            className="absolute left-[630px] bottom-[185px] w-16"
+            className="absolute left-[630px] bottom-[185px] ml-auto w-16"
           />
         </button>
       </div>
