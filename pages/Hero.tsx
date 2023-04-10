b4a57a70dc1ca82e8820ab3ee705131472a29bb9
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-04-10 09:33:45 -0700

    set margins left and right to auto

--- pages/Hero.tsx
+++ pages/Hero.tsx
@@ -8,21 +8,26 @@ export default function Hero() {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
   return (
-    <section className="pb-29 flex max-w-[90rem] pl-[8rem]">
-      <div id="top" className="pr-[8rem]">
+    // max-w-[90rem]
+    <section className="pb-29 mx-auto flex max-w-[90rem] pl-[8rem]">
+      <div id="top" className=" pr-[8rem]">
         {/* change width depending on scaling max-w[35rem]*/}
         <h1 className="mb-11 w-[35rem] pt-20 pb-[3rem] font-dmserif text-8xl">
           Paid training to promote equity and progress
         </h1>
         {/* scaling max-w-[25rem] */}
-        <p className=" w-[25rem] font-montserrat text-3xl font-medium leading-10 text-spec-turquiose">
+        <p className=" max-w-[25rem] font-montserrat text-3xl font-medium leading-10 text-spec-turquiose">
           SPEC provides paid learning opportunities by partnering with clients
           to build websites, hardware, art, and written content.
         </p>
       </div>
       {/* scaling min-h-[55rem] max-w-[45rem] */}
       <div className="relative">
-        <Image src={training} alt="training-photo" className="min-w-[45rem]" />
+        <Image
+          src={training}
+          alt="training-photo"
+          className="mt-0 min-h-[55rem] max-w-[45rem]"
+        />
         <button onClick={toTop}>
           <Image
             src={topButton}
