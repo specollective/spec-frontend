9f22e115c4ebf621e0d293abee0c6b06add5bc35
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-04-13 10:53:30 -0700

    placed text and image in div, fixed text scaling

--- pages/Hero.tsx
+++ pages/Hero.tsx
@@ -8,28 +8,30 @@ export default function Hero() {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
   return (
-    <section className="flex ">
-      <div
-        id="top"
-        className="md:pl-18 w-2/5 pl-8 md:mt-10 md:ml-12 lg:mt-[6rem] xl:pl-[16rem] 2xl:w-6/12"
-      >
-        <h1 className="xl:text-5x mt-3 font-dmserif text-base md:text-3xl lg:text-5xl 2xl:text-7xl">
-          Paid training to promote equity and progress
-        </h1>
-        <p className="mt-2 w-8/12 font-montserrat text-xs text-spec-turquiose sm:text-xl md:mt-5 md:w-4/5 md:text-lg lg:mt-8 lg:text-2xl xl:text-3xl 2xl:text-4xl">
-          SPEC provides paid learning opportunities by partnering with clients
-          to build websites, hardware, art, and written content.
-        </p>
-      </div>
-      <div className="relative top-1/2 h-full w-3/5 md:pl-8 2xl:w-6/12 2xl:p-0">
-        <Image src={training} alt="training-photo" className="float-right" />
-        <button onClick={toTop} className="invisible lg:visible">
-          <Image
-            src={topButton}
-            alt="back-to-top"
-            className="absolute right-10 bottom-32 ml-auto w-full w-16 -translate-y-1/2 "
-          />
-        </button>
+    <section className="">
+      <div className="flex">
+        <div
+          id="top"
+          className="w-2/5 pl-8 md:mt-10 md:ml-12 md:pl-10 lg:mt-[6rem]  2xl:w-6/12"
+        >
+          <h1 className="mt-3 font-dmserif text-lg md:text-3xl lg:text-5xl xl:text-7xl 2xl:text-8xl">
+            Paid training to promote equity and progress
+          </h1>
+          <p className="mt-2 w-8/12 font-montserrat text-xs text-spec-turquiose md:mt-5 md:w-4/5 md:text-base lg:mt-8 lg:text-xl xl:text-2xl 2xl:text-3xl">
+            SPEC provides paid learning opportunities by partnering with clients
+            to build websites, hardware, art, and written content.
+          </p>
+        </div>
+        <div className="relative top-1/2 inline h-full w-3/5 md:pl-8 2xl:w-6/12">
+          <Image src={training} alt="training-photo" className="float-right" />
+          <button onClick={toTop} className="invisible lg:visible">
+            <Image
+              src={topButton}
+              alt="back-to-top"
+              className="absolute right-10 bottom-32 ml-auto w-full w-16 -translate-y-1/2 "
+            />
+          </button>
+        </div>
       </div>
     </section>
   );
