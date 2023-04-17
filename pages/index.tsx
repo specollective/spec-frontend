6ec29396100915c7f628974010c521019e5e6de6
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-04-17 12:07:12 -0700

    removed getinvolved from wrong branch, added it back in

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Navbar from "./Navbar";
+import GetInvolved from "./GetInvolved";
 
 export default function Home() {
   return (
