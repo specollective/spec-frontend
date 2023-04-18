d5e11c87f95008a5a97a8fe2884d4075fbb12fff
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-04-18 14:46:32 -0700

    added Hero component and imported to home

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,12 +1,11 @@
 import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
 
 export default function Home() {
-     
-    return (
-        <>
-            <Navbar />
-        </>
-        
-    )
-
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
 }
