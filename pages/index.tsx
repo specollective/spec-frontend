ee02a3ff3306b38c7ec656b84ebfca63d52197e7
najwanan <66931010+najwanan@users.noreply.github.com>
2023-04-18 12:52:52 -0700

    adding home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import Navbar from "./Navbar";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
