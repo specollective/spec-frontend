81e00e6b432743c35f9e9914f8bffd2b2fa8cb00
Joe Torreggiani <joetorreggiani@gmail.com>
2023-04-25 21:14:30 -0400

    Fix typescript errors.

--- pages/stat-box.tsx
+++ pages/stat-box.tsx
@@ -1,10 +1,18 @@
-export default function StatBox({ height, width, className, children }) {
-    return (
-        <div
-            className={`${className} flex flex-col  ${height} ${width} justify-center text-center p-4`}
-        >
-            {/* <p className={`text-${statColor} text-7xl font-semibold mb-5`}>{inputStat}</p> */}
-            {children}
-        </div>
-    );
+import { ReactNode } from 'react';
+interface StatBoxProps {
+  height: string;
+  width: string;
+  className: string;
+  children: ReactNode;
+}
+
+export default function StatBox({ height, width, className, children }: StatBoxProps) {
+  return (
+    <div
+      className={`${className} flex flex-col ${height} ${width} justify-center text-center p-4`}
+    >
+      {/* <p className={`text-${statColor} text-7xl font-semibold mb-5`}>{inputStat}</p> */}
+      {children}
+    </div>
+  );
 }
