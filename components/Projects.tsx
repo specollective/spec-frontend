f04a2d9168f48e8cb4e61e98d1fced880129b4e3
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-04-26 13:06:20 -0700

    finished styling, browser and mobile responsive

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,22 +1,36 @@
 import React from "react";
 import Image from "next/image";
-import projectsImage from "../publicProjectsImg.svg";
+import projectsImage from "../public/ProjectsImg.svg";
 
 export default function Projects() {
 	return (
-		<section>
-			<div>
-				<h1>PROJECTS</h1>
-				<p>
-					SPEC is organized as an open collective, which raises and spends funds
-					transparently to achieve its goals.
-				</p>
-				<p>
-					Your donations help Research Associates &#40;RAs&#41; receive
-					mentoring, career training, and professional development as they work
-					on these current projects.
-				</p>
+		<div>
+			<div className="flex flex-col-reverse md:flex-row items-center justify-center ml-20 ">
+				<div id="container-left" className="md:pr-10 w-10/12 md:w-6/12">
+					<h1 className="font-montserrat font-medium text-sm leading-5 opacity-40 tracking-wider mb-4 md:text-lg md:leading-7">
+						PROJECTS
+					</h1>
+					<p className="font-dmserif text-base font-normal leading-8 mb-6 md:text-4xl md:leading-10">
+						SPEC is organized as an open collective, which
+						<span className="text-spec-turquiose">
+							&nbsp;raises and spends funds transparently&nbsp;
+						</span>
+						to achieve its goals.
+					</p>
+					<p className="font-montserrat opacity-70 font-medium text-base leading-nromal md:text-lg md:leading-7">
+						Your donations help Research Associates &#40;RAs&#41; receive
+						mentoring, career training, and professional development as they
+						work on these current projects.
+					</p>
+				</div>
+				<div id="right-container" className="w-7/12">
+					<Image
+						className="float-right scale-100 pl-10 ml-10"
+						src={projectsImage}
+						alt="Interacting with web browsers"
+					/>
+				</div>
 			</div>
-		</section>
+		</div>
 	);
 }
