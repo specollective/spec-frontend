81cd9585f018d2b821541c3ea4085ebf1b18cc97
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-04-26 12:00:36 -0700

    established Projects.tsx skeleton outline

---
+++ components/Projects.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "next/image";
+import projectsImage from "../publicProjectsImg.svg";
+
+export default function Projects() {
+	return (
+		<section>
+			<div>
+				<h1>PROJECTS</h1>
+				<p>
+					SPEC is organized as an open collective, which raises and spends funds
+					transparently to achieve its goals.
+				</p>
+				<p>
+					Your donations help Research Associates &#40;RAs&#41; receive
+					mentoring, career training, and professional development as they work
+					on these current projects.
+				</p>
+			</div>
+		</section>
+	);
+}
