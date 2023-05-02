5834189db26d7153773a11dc9b03d23ab419a40f
najwanan <66931010+najwanan@users.noreply.github.com>
2023-05-02 17:42:10 -0400

    adding projects content

--- pages/home.tsx
+++ pages/home.tsx
@@ -6,7 +6,6 @@ import React from "react";
 import FormDiv from '../components/FormDiv'
 import ProjectsSection from "../components/ProjectsSection";
 import Mission from '../components/Mission'
-// import ProjectsAccordion from "../components/ProjectsAccordian";
 import Projects from "../components/Projects";
 
 export default function Home() {
@@ -16,6 +15,7 @@ export default function Home() {
       <Hero />
       <ProjectsSection />
       <Mission />
+      <Projects />
       <ByTheNumbers />
       <FormDiv />
       {/* <Testimonials/> */}
