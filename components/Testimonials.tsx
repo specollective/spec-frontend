73f8de4f73df915e5a547f9505f1f7d11e639424
Joe Torreggiani <joetorreggiani@gmail.com>
2023-05-02 15:15:04 -0400

    Get working carousel to build off from.

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -22,30 +22,124 @@ export default function Testimonials() {
   ]
 
   const [activeSlide, setActiveSlide] = useState(0);
-  const testimonialCards = testimonialData.map((testimonialCard, index) => {{
-    return (
+  
+  return (
+    <div className="w-1/2 m-auto p-10">
       <div
-        key={index}
-        className="relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-        data-te-carousel-item
+        id="carouselTestimonialsIndicators"
+        className="relative"
+        data-te-carousel-init
+        data-te-carousel-slide
       >
-        <div className="w-full h-full">
-          <div id="borderBox" className="border-solid border-spec-turquiose border-8 py-8 my-8 font-montserrat text-base text-center w-1/3">
-            <Image src={testimonialCard.profileImage} alt="Profile picture" className="rounded-full border-2" width={50} height={50} />
-            {/* <Image src="" alt="Quote image"  /> */}
-            <p id="quotations" className="py-6">{testimonialCard.quoteBox}</p>
-            {/* <hr id="line" className="color-black w-10 "></hr> */}
-            <div className="flex-grow border-t-4 border-spec-sunshine"></div>
-            <p id="name" className="text-lg font-medium py-6">{testimonialCard.name}</p>
+        <div
+          className="absolute bottom-0 left-0 right-0 z-[2] mx-[15%] mb-4 flex list-none justify-center p-0"
+          data-te-carousel-indicators>
+          <button
+            type="button"
+            data-te-target="#carouselTestimonialsIndicators"
+            data-te-slide-to="0"
+            data-te-carousel-active
+            className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+            aria-current="true"
+            aria-label="Slide 1"></button>
+          <button
+            type="button"
+            data-te-target="#carouselTestimonialsIndicators"
+            data-te-slide-to="1"
+            className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+            aria-label="Slide 2"></button>
+          <button
+            type="button"
+            data-te-target="#carouselTestimonialsIndicators"
+            data-te-slide-to="2"
+            className="mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-white bg-clip-padding p-0 -indent-[999px] opacity-50 transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+            aria-label="Slide 3"></button>
+        </div>
+
+        {/* <!--Carousel items--> */}
+        <div
+          className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
+          {/* <!--First item--> */}
+          <div
+            className="relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+            data-te-carousel-item
+            data-te-carousel-active
+          >
+            <div className="block w-full h-60 bg-gray-300">
+              Testimonial #1
+            </div>
+          </div>
+          {/* <!--Second item--> */}
+          <div
+            className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+            data-te-carousel-item
+          >
+            <div className="block w-full h-60 bg-gray-300">
+            Testimonial #2
+            </div>
+          </div>
+          {/* <!--Third item--> */}
+          <div
+            className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+            data-te-carousel-item
+          >
+            <div className="block w-full h-60 bg-gray-300">
+            Testimonial #3
+            </div>
           </div>
         </div>
-      </div>
-    )
-  }})
 
-  return (
-    <div className="flex justify-center items-center">
-      {testimonialCards}
+        {/* <!--Carousel controls - prev item--> */}
+        <button
+          className="absolute bottom-0 left-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+          type="button"
+          data-te-target="#carouselTestimonialsIndicators"
+          data-te-slide="prev">
+          <span className="inline-block h-8 w-8">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="h-6 w-6">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.75 19.5L8.25 12l7.5-7.5" />
+            </svg>
+          </span>
+          <span
+            className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+          >
+            Previous
+          </span>
+        </button>
+        {/* <!--Carousel controls - next item--> */}
+        <button
+          className="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+          type="button"
+          data-te-target="#carouselTestimonialsIndicators"
+          data-te-slide="next">
+          <span className="inline-block h-8 w-8">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="h-6 w-6">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+            </svg>
+          </span>
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Next
+          </span>
+        </button>
+      </div>
     </div>
   )
 }
