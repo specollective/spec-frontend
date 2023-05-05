0b4bdc6b31bae54f23eb99062a42d67c0a2af4b0
emmacodes <binma.emma@gmail.com>
2023-05-04 21:18:10 -0700

    connect to links and open in new tabs

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -18,8 +18,8 @@ export default function Navbar() {
             </div>
 
             <div className='flex justify-center items-center space-x-8 font-montserrat leading-5 text-lg mb-6'>
-                <a href='#' className='font-medium text-center hover:underline hover:font-semibold tracking-wide'>Documentation</a>
-                <a href='#' className='font-bold border border-spec-turquiose border-4 bg-spec-turquiose tracking-wide text-center py-5 px-10 text-sm p-4 text-white rounded-br-2xl rounded-tl-2xl font-montserrat w-3/4 hover:text-black hover:bg-white hover:border-spec-turquiose'>
+                <a href='https://docs.specollective.org/introduction/' target='_blank' rel="noreferrer" className='font-medium text-center hover:underline hover:font-semibold tracking-wide'>Documentation</a>
+                <a href='https://opencollective.com/spec' target='_blank' rel="noreferrer" className='font-bold border border-spec-turquiose border-4 bg-spec-turquiose tracking-wide text-center py-5 px-10 text-sm p-4 text-white rounded-br-2xl rounded-tl-2xl font-montserrat w-3/4 hover:text-black hover:bg-white hover:border-spec-turquiose'>
                     DONATE</a>
             </div>
         </div>
