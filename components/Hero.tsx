1712e5d9e4e87b8b8e5a7aadf034f4ffe9052b04
Shane Jeon <88866019+codingtokki@users.noreply.github.com>
2023-05-09 11:38:37 -0700

    added 'left:0' to scroll-to-top function, button was getting stuck on certain sections of page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,27 +5,27 @@ import topButton from "../public/Back to Top.svg";
 
 export default function Hero() {
 	const toTop = () => {
-		window.scrollTo({ top: 0, behavior: "smooth" });
+		window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
 	};
 	return (
 		<section className="">
-			<div className="lg:min-h-screen flex justify-end">
+			<div className="flex justify-end lg:min-h-screen">
 				<div
 					id="top"
-					className="w-1/2 ml-10 mt-4 mb-1 md:ml-20 md:mt-8 md:w-5/12 lg:mt-16 xl:ml-36 pl-10 lg:pl-2">
-					<h1 className="font-dmserif text-2xl w-10/12 md:text-5xl lg:text-6xl xl:text-7xl lg:w-9/12 2xl:text-8xl 2xl:w-11/12 xl:w-9/12 lg:leading-tight">
+					className="mb-1 ml-10 mt-4 w-1/2 pl-10 md:ml-20 md:mt-8 md:w-5/12 lg:mt-16 lg:pl-2 xl:ml-36">
+					<h1 className="w-10/12 font-dmserif text-2xl md:text-5xl lg:w-9/12 lg:text-6xl lg:leading-tight xl:w-9/12 xl:text-7xl 2xl:w-11/12 2xl:text-8xl">
 						Paid training to promote equity and progress
 					</h1>
 					<p
 						id="browser-text"
-						className="font-montserrat text-spec-turquiose hidden lg:block text-2xl font-medium lg:w-8/12 lg:text-2xl xl:text-3xl mt-10 xl:w-8/12 2xl:text-4xl 2xl:w-9/12 2xl:mt-16 2xl:leading-normal 2xl:mb-18">
+						className="2xl:mb-18 mt-10 hidden font-montserrat text-2xl font-medium text-spec-turquiose lg:block lg:w-8/12 lg:text-2xl xl:w-8/12 xl:text-3xl 2xl:mt-16 2xl:w-9/12 2xl:text-4xl 2xl:leading-normal">
 						Through partnerships with clients, SPEC offers paid learning
 						opportunities in website development, hardware creation, art, and
 						writing.
 					</p>
 					<p
 						id="mobile-text"
-						className="font-medium text-sm block lg:hidden font-montserrat text-spec-turquiose w-8/12 mt-4 pb-20 md:text-2xl md:w-9/12 md:mt-8 md:leading-8">
+						className="mt-4 block w-8/12 pb-20 font-montserrat text-sm font-medium text-spec-turquiose md:mt-8 md:w-9/12 md:text-2xl md:leading-8 lg:hidden">
 						SPEC provides paid learning opportunities by partnering with clients
 						to build websites, hardware, art, and written content.
 					</p>
@@ -45,7 +45,7 @@ export default function Hero() {
 					<Image
 						src={topButton}
 						alt="back-to-top"
-						className="fixed right-10 -bottom-2 xl:-translate-y-1/2 "
+						className="fixed -bottom-2 right-10 xl:-translate-y-1/2 "
 					/>
 				</button>
 			</div>
