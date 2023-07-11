6d818e18507b7469c766f6a19e1277683e4ff338
najwanan <66931010+najwanan@users.noreply.github.com>
2023-07-12 02:23:04 +0300

    formatting donate button

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,17 +4,17 @@ import SocialMedia from './SocialMedia'
 export default function Footer() {
   return (
     <footer className="bg-[#0C9FAA] h-80">
-      <section className="flex p-14 justify-evenly">
-      <div className="flex-row ">
+      <section className="flex p-4 justify-evenly">
+      <div className="flex-row">
         <div className="text-center mt-12 px-4">
           <a
             href="https://opencollective.com/spec"
-            className="  block bg-spec-yellow  text-center ml-24 p-3.5 text-md font-medium text-black rounded-br-lg rounded-tl-lg font-montserrat h-12 w-28 "
+            className="  block bg-spec-yellow  text-center ml-24 p-3.5 text-md font-medium text-black rounded-br-2xl rounded-tl-2xl   hover:text-black hover:bg-white hover:border-spec-turquiose font-montserrat h-12 w-28 "
           >
             DONATE
           </a>
           </div>
-            <div className=" lg:hidden items-center mt-10">
+          <div className=" lg:hidden content-center mt-10">
          <SocialMedia/>
         </div> 
         <div className=" lg:block items-center mt-10">
