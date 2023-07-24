d8fe689543a4ee1911e7fe4309704831daafc3c7
najwanan <66931010+najwanan@users.noreply.github.com>
2023-07-24 22:54:17 +0300

    formatting carousel slides with lineclamp and adjusting font

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -35,7 +35,7 @@ export default function Testimonials() {
 
         {/* <!--Carousel items--> */}
         <div
-          className="flex justify-evenly w-full overflow-hidden after:clear-both after:block after:content-['']">
+          className="flex justify-evenly w-full md:h-104 overflow-hidden after:clear-both after:block after:content-['']">
           {/* <!--First item--> */}
           <div
             className="flex content-center -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
@@ -43,7 +43,7 @@ export default function Testimonials() {
             data-te-carousel-active
           >
            
-            <div className="-z-10 border-solid rounded border-spec-turquiose border-2 py-8  font-montserrat items-center text-base text-center w-3/4 lg:w-1/2">
+            <div className="-z-10 border-solid rounded border-spec-turquiose border-2 py-8  font-montserrat items-center text-base text-center w-4/5 md:w-1/2">
               <div className="flex justify-center">
               <Image
               className=" z-10 flex justify-center -mt-14 rounded-full"
@@ -55,7 +55,7 @@ export default function Testimonials() {
            
             <blockquote>
              <DownQuotation/>
-            <p className="text-md md:text-lg font-montserrat font-light italic p-10  text-gray-900 dark:text-black sm:text-ellipsis sm:overflow-hidden">During my time at SPEC, I learned so much about the foundations of software engineering, agile, and open source development. I joined SPEC for it&apos;s mission to empower individuals to learn as you go and contribute to world-changing initiatives through the open sharing economy. With a small but mighty team, the genuine care that everyone has to do good by people is what keeps me motivated to continue contributing, growing, and learning</p>
+            <p className="text-md md:text-lg font-montserrat font-light italic mb-4 p-10  text-gray-900 dark:text-black sm:text-ellipsis sm:overflow-hidden line-clamp-4">During my time at SPEC, I learned so much about the foundations of software engineering, agile, and open source development. I joined SPEC for it&apos;s mission to empower individuals to learn as you go and contribute to world-changing initiatives through the open sharing economy. With a small but mighty team, the genuine care that everyone has to do good by people is what keeps me motivated to continue contributing, growing, and learning</p>
                      
             <div className="visible md:hidden">
               <UpQuotation/>
@@ -64,17 +64,18 @@ export default function Testimonials() {
             </blockquote> 
               
               <div className=" flex-shrink border-t-2 ml-20 mr-20 w-30 border-spec-sunshine "></div>
-              <p id="name" className="content-center text-xl font-semibold py-6">VICTORIA LO</p>
+              <p id="name" className="content-center text-xl font-medium tracking-widest font-poppins py-6">VICTORIA LO</p>
               {/* <p id="title" className="content-center text-lg font-normal py-6">Research Associate</p> */}
             </div>
           </div>
 
           {/* <!--Second item--> */}
           <div
-            className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+            className="relative float-left -mr-[100%] hidden w-full md:h-104 transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
             data-te-carousel-item
           >
             <div className="border-solid rounded border-spec-turquiose border-2 py-8 my-8 font-montserrat items-center text-base text-center w-1/2">
+             
             <div className="flex justify-center">
               <Image
               className=" z-10 w-28 h-28 flex justify-center -mt-14 rounded-full "
@@ -86,7 +87,7 @@ export default function Testimonials() {
             
             <blockquote>
              <DownQuotation/>
-                <p className="text-md md:text-lg font-montserrat font-light italic p-6  text-gray-900 dark:text-black">SPEC has been a perfect next step in my UX design journey. It&apos;s such a great space for me to explore the different areas within UX design, to figure out where I excel. On top of discovery within this new field for me, being 100% supported and encouraged to continue learning has been amazing. SPEC is going to change the world, and I am happy to contribute my small piece!
+                <p className="text-md md:text-lg font-montserrat font-light italic p-6 mb-4 text-gray-900 dark:text-black line-clamp-4">SPEC has been a perfect next step in my UX design journey. It&apos;s such a great space for me to explore the different areas within UX design, to figure out where I excel. On top of discovery within this new field for me, being 100% supported and encouraged to continue learning has been amazing. SPEC is going to change the world, and I am happy to contribute my small piece!
                 </p>
 
                 <div className="visible md:hidden">
@@ -95,16 +96,16 @@ export default function Testimonials() {
            
             </blockquote> 
               <div className=" flex-shrink border-t-2 ml-20 mr-20 w-30 border-spec-sunshine "></div>
-              <p id="name" className="text-xl font-semibold py-6">ILIA DE LEON</p>
+              <p id="name" className="text-xl font-medium tracking-widest py-6">ILIA DE LEON</p>
                {/* <p id="title" className="content-center text-lg font-normal py-6">Research Associate</p> */}
             </div>
           </div>
           {/* <!--Third item--> */}
           <div
-            className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+            className="relative float-left -mr-[100%] hidden w-full md:h-104 transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
             data-te-carousel-item
           >
-            <div className="border-solid rounded border-spec-turquiose border-2 py-8 my-8 font-montserrat items-center text-base text-center w-1/2">
+            <div className="border-solid rounded border-spec-turquiose border-2 py-8 my-8 font-montserrat items-center text-base text-center  w-1/2">
              <div className="flex justify-center">
               <Image
               className=" z-10 w-28 h-28 flex justify-center -mt-14 rounded-full"
@@ -114,8 +115,9 @@ export default function Testimonials() {
               />  
             </div>
             <blockquote>
-             <DownQuotation/>
-                <p className="text-md md:text-lg font-montserrat font-light italic p-6  text-gray-900 dark:text-black">As an RA for SPEC, I had the opportunity to dive deep into the world of coding and gain valuable skills that have had a lasting impact on my personal and professional growth. But beyond just the technical knowledge, what truly stood out to me was the incredibly supportive and kind community of people at SPEC. I felt welcomed from day one and was always encouraged to ask questions, seek guidance, and push myself to learn more. The experience of working with such a talented and supportive group of individuals has had a profound impact on me, and I&apos;m grateful for the time I spent at SPEC.</p>
+                <DownQuotation />
+                
+                <p className="text-md md:text-lg font-montserrat font-light italic p-6  text-gray-900 dark:text-black line-clamp-4">As an RA for SPEC, I had the opportunity to dive deep into the world of coding and gain valuable skills that have had a lasting impact on my personal and professional growth. But beyond just the technical knowledge, what truly stood out to me was the incredibly supportive and kind community of people at SPEC. I felt welcomed from day one and was always encouraged to ask questions, seek guidance, and push myself to learn more. The experience of working with such a talented and supportive group of individuals has had a profound impact on me, and I&apos;m grateful for the time I spent at SPEC.</p>
                 
               <div className="visible md:hidden">
                 <UpQuotation/>
@@ -123,13 +125,13 @@ export default function Testimonials() {
            
           </blockquote> 
               <div className=" flex-shrink border-t-2 ml-20 mr-20 w-30 border-spec-sunshine "></div>
-              <p id="name" className="text-xl font-semibold py-6">YOLINE BANERJEE</p>
+              <p id="name" className="text-xl font-medium tracking-widest py-6">YOLINE BANERJEE</p>
                {/* <p id="title" className="content-center text-lg font-normal py-6">Research Associate</p> */}
             </div>
           </div>
              {/* <!--Fourth item--> */}
           <div
-            className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+            className="relative float-left -mr-[100%] hidden w-full md:h-104 transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
             data-te-carousel-item
           >
             <div className="border-solid rounded border-spec-turquiose border-2 py-8 my-8 font-montserrat items-center text-base text-center w-1/2">
@@ -142,8 +144,9 @@ export default function Testimonials() {
                 /> 
             </div>
             <blockquote>
-             <DownQuotation/>
-                <p className="text-md md:text-lg font-montserrat font-light italic p-6  text-gray-900 dark:text-black">I joined SPEC during a transitionary period of my life. Being able to work on multiple projects and in various roles all while being mentored helped me identify what my next career choice should be. Now that I have found gainful employment outside of SPEC I am happy to still feel included and connected to the team.</p>
+                <DownQuotation />
+                {/*write a conditional that will line clamp the paragraph after 5 lines*/}
+                <p className="text-md md:text-lg font-montserrat font-light italic p-6  text-gray-900 dark:text-black line-clamp-4">I joined SPEC during a transitionary period of my life. Being able to work on multiple projects and in various roles all while being mentored helped me identify what my next career choice should be. Now that I have found gainful employment outside of SPEC I am happy to still feel included and connected to the team.</p>
 
               <div className="visible md:hidden">
                    <UpQuotation/>
@@ -151,7 +154,7 @@ export default function Testimonials() {
            
           </blockquote> 
               <div className=" flex-shrink border-t-2 ml-20 mr-20 w-30 border-spec-sunshine "></div>
-              <p id="name" className="text-xl font-semibold py-6">ALICIA BONG</p>
+              <p id="name" className="text-xl font-medium tracking-widest py-6">ALICIA BONG</p>
                {/* <p id="title" className="content-center text-lg font-normal py-6">Research Associate</p> */}
             </div>
           </div>
