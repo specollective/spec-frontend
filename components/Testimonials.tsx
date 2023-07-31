bdd8d1486fab0e597450990b74f3617bdae94d3c
najwanan <66931010+najwanan@users.noreply.github.com>
2023-07-31 16:13:29 +0300

    refactoring to create constants for style classes fortestimonial elements

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -17,8 +17,47 @@ export default function Testimonials() {
 		initTE({ Carousel });
 	}, []);
   
- 
+  const [clamped, setClamped] = useState(true);
+  const [showButton, setShowButton] = useState(true);
+
+  const handleClick = () => setClamped(!clamped);
   const [activeSlide, setActiveSlide] = useState(0);
+
+  //create a constant that will hold the styles for the p tag in the carousel
+
+  const quoteStyle = `
+  text-md 
+  md:text-lg 
+  font-montserrat 
+  font-light 
+  italic 
+  mb-4 p-6  
+  text-gray-900 
+  dark:text-black 
+  sm:text-ellipsis 
+  sm:overflow-hidden 
+  line-clamp-4
+  `
+
+  //create a constant that will hold the tailwind classes for cards in the carousel
+
+  const testimonialCardStyle = `
+  -z-10 
+  border-solid 
+  rounded 
+  border-spec-turquiose 
+  border-2 
+  py-8 
+  my-8 
+  font-montserrat 
+  items-center 
+  text-base 
+  text-center 
+  w-full 
+  md:w-1/2
+  `
+ 
+
 
 
   return (
@@ -42,7 +81,7 @@ export default function Testimonials() {
             data-te-carousel-item
             data-te-carousel-active
           >
-            <div className="-z-10 border-solid rounded border-spec-turquiose border-2 py-8 my-8 font-montserrat items-center text-base text-center w-full md:w-1/2">
+            <div className={testimonialCardStyle}>
               <div className="flex justify-center">
               <Image
               className="z-10 flex justify-center -mt-14 rounded-full"
@@ -53,14 +92,14 @@ export default function Testimonials() {
             </div>
             <blockquote>
              <DownQuotation/>
-            <p className="text-md md:text-lg font-montserrat font-light italic mb-4 p-6  text-gray-900 dark:text-black sm:text-ellipsis sm:overflow-hidden line-clamp-4">During my time at SPEC, I learned so much about the foundations of software engineering, agile, and open source development. I joined SPEC for it&apos;s mission to empower individuals to learn as you go and contribute to world-changing initiatives through the open sharing economy. With a small but mighty team, the genuine care that everyone has to do good by people is what keeps me motivated to continue contributing, growing, and learning</p>
+            <p className={quoteStyle}>During my time at SPEC, I learned so much about the foundations of software engineering, agile, and open source development. I joined SPEC for it&apos;s mission to empower individuals to learn as you go and contribute to world-changing initiatives through the open sharing economy. With a small but mighty team, the genuine care that everyone has to do good by people is what keeps me motivated to continue contributing, growing, and learning</p>
             <div className="visible md:hidden">
               <UpQuotation/>
             </div> 
             
             </blockquote> 
               <div className=" flex-shrink border-t-2 pt-8 ml-20 mr-20 w-30 border-spec-sunshine "></div>
-              <p id="name" className="content-center text-xl font-medium tracking-widest font-poppins py-6">VICTORIA LO</p>
+              <p id="name" className="content-center text-xl font-medium tracking-widest font-poppins">VICTORIA LO</p>
               {/* <p id="title" className="content-center text-lg font-normal py-6">Research Associate</p> */}
             </div>
           </div>
@@ -72,7 +111,7 @@ export default function Testimonials() {
           >
       
 
-            <div className="border-solid rounded border-spec-turquiose border-2 py-8 my-8 font-montserrat items-center text-base text-center w-full md:w-1/2">
+            <div className={testimonialCardStyle}>
              
             <div className="flex justify-center">
               <Image
@@ -85,7 +124,7 @@ export default function Testimonials() {
             
             <blockquote>
              <DownQuotation/>
-                <p className="text-md md:text-lg font-montserrat font-light italic p-6 mb-4 text-gray-900 dark:text-black line-clamp-4">SPEC has been a perfect next step in my UX design journey. It&apos;s such a great space for me to explore the different areas within UX design, to figure out where I excel. On top of discovery within this new field for me, being 100% supported and encouraged to continue learning has been amazing. SPEC is going to change the world, and I am happy to contribute my small piece!
+                <p className={quoteStyle}>SPEC has been a perfect next step in my UX design journey. It&apos;s such a great space for me to explore the different areas within UX design, to figure out where I excel. On top of discovery within this new field for me, being 100% supported and encouraged to continue learning has been amazing. SPEC is going to change the world, and I am happy to contribute my small piece!
                 </p>
 
                 <div className="visible md:hidden">
@@ -104,7 +143,7 @@ export default function Testimonials() {
             data-te-carousel-item
           >
             
-            <div className="-z-10 border-solid rounded border-spec-turquiose border-2 py-8 my-8 font-montserrat items-center text-base text-center w-full md:w-1/2">
+            <div className={testimonialCardStyle}>
              <div className="flex justify-center">
               <Image
               className=" z-10 w-28 h-28 flex justify-center -mt-14 rounded-full"
@@ -116,7 +155,7 @@ export default function Testimonials() {
             <blockquote>
                 <DownQuotation />
                 
-                <p className="text-md md:text-lg font-montserrat font-light italic p-6  text-gray-900 dark:text-black line-clamp-4">As an RA for SPEC, I had the opportunity to dive deep into the world of coding and gain valuable skills that have had a lasting impact on my personal and professional growth. But beyond just the technical knowledge, what truly stood out to me was the incredibly supportive and kind community of people at SPEC. I felt welcomed from day one and was always encouraged to ask questions, seek guidance, and push myself to learn more. The experience of working with such a talented and supportive group of individuals has had a profound impact on me, and I&apos;m grateful for the time I spent at SPEC.</p>
+                <p className={quoteStyle}>As an RA for SPEC, I had the opportunity to dive deep into the world of coding and gain valuable skills that have had a lasting impact on my personal and professional growth. But beyond just the technical knowledge, what truly stood out to me was the incredibly supportive and kind community of people at SPEC. I felt welcomed from day one and was always encouraged to ask questions, seek guidance, and push myself to learn more. The experience of working with such a talented and supportive group of individuals has had a profound impact on me, and I&apos;m grateful for the time I spent at SPEC.</p>
                 
               <div className="visible md:hidden">
                 <UpQuotation/>
@@ -134,7 +173,7 @@ export default function Testimonials() {
             data-te-carousel-item
           >
       
-            <div className="border-solid rounded border-spec-turquiose border-2 py-8 my-8 font-montserrat items-center text-base text-center w-full md:w-1/2">
+            <div className={testimonialCardStyle}>
              <div className="flex justify-center">
               <Image
               className="z-10 w-28 h-28 flex justify-center -mt-14 rounded-full"
@@ -145,7 +184,7 @@ export default function Testimonials() {
             </div>
             <blockquote>
                 <DownQuotation />
-                <p className="text-md md:text-lg font-montserrat font-light italic p-6  text-gray-900 dark:text-black line-clamp-4">I joined SPEC during a transitionary period of my life. Being able to work on multiple projects and in various roles all while being mentored helped me identify what my next career choice should be. Now that I have found gainful employment outside of SPEC I am happy to still feel included and connected to the team.</p>
+                <p className={quoteStyle}>I joined SPEC during a transitionary period of my life. Being able to work on multiple projects and in various roles all while being mentored helped me identify what my next career choice should be. Now that I have found gainful employment outside of SPEC I am happy to still feel included and connected to the team.</p>
 
               <div className="visible md:hidden">
                    <UpQuotation/>
