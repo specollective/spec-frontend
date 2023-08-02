eed3c70e7d5a53e9ad68142c42b221dc6aba7601
Joe Torreggiani <joetorreggiani@gmail.com>
2023-08-01 22:00:05 -0400

    Add comments and reference for using environmental variables.

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -6,6 +6,15 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const body = req.body
 
+  // Use these environment variables to configure NodeMailer
+  // In development you can create a .env.local file with these variables
+  // Add use your own email to test that it works. The production values
+  // will be set in DigitalOcean App Platform.
+  if (process.env.NODE_ENV === 'development') {
+    console.log(process.env.NODE_MAILER_EMAIL)
+    console.log(process.env.NODE_MAILER_PASSWORD)
+  }
+
   // response return 200 status code and json data with body object, fullName, reason, email and message
   res.status(201).json({
     data: `${body.fullName} ${body.email} ${body.reason} ${body.message}`
