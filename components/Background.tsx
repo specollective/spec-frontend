64a2eaf6bb1c8a9d9f25de04dc7e31118500731f
najwanan <66931010+najwanan@users.noreply.github.com>
2023-08-07 23:26:01 +0300

    updating content and fixing margins

--- components/Background.tsx
+++ components/Background.tsx
@@ -6,7 +6,7 @@ export default function Background() {
   return (
     <section>
       <div className="flex flex-col md:flex md:flex-row m-20 items-center justify-center">
-        <div className="md:w-1/3 md:pl-10 md:order-last">
+        <div className="md:w-1/2 md:order-last">
           <Image
             className="m-auto text-left mb-6 hidden md:block md:order-last"
             src={bpImg}
@@ -22,7 +22,7 @@ export default function Background() {
             height={100}
           />
         </div>
-        <div className="md:w-1/2 md:order-first">
+        <div className="md:w-1/2 md:order-last">
           <p className="font-montserrat font-medium text-sm leading-5 opacity-40 tracking-wider mb-4 md:text-lg md:leading-7">
             OUR HISTORY
           </p>
@@ -30,10 +30,10 @@ export default function Background() {
             Uncovering a Cultural Gap: <span className="text-spec-turquiose">The Buffalo Project’s 10-Year data analysis reveals alarming lack of diversity awareness</span> among students.
           </h1>
           <p className="font-montserrat opacity-70 font-medium text-base leading-normal mb-4 md:text-lg md:leading-7">
-            This illustrates that students were not being adequately taught the importance of intercultural competencies and cross-cultural communication.
+              This illustrates that students  were not being adequately taught the importance of intercultural competencies and cross-cultural communication.
           </p>
           <p className="font-montserrat opacity-70 font-medium text-base leading-normal mb-4 md:text-lg md:leading-7">
-            The Buffalo Project, led by Dr. Rhianna C. Rogers, worked with community stakeholders and WNY data to engage in a participatory action research study of perceptions of culture in the region.
+            The Buffalo Project, led by Dr. Rhianna C. Rogers, worked with community stakeholders and WNY data to engage in a participatory action research study of perceptions of culture in the region. 
           </p>
           <a
             href="https://thebuffaloproject.wixsite.com/tbpinternship"
