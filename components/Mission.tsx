8073ee71c755a611ec6ad591e65931fba6185178
najwanan <66931010+najwanan@users.noreply.github.com>
2023-08-07 23:09:57 +0300

    updating content and margins

--- components/Mission.tsx
+++ components/Mission.tsx
@@ -5,7 +5,18 @@ import missionImg from '../public/MissionImg.svg'
 export default function Mission() {
   return (
     <div className='flex flex-col md:flex md:flex-row m-20 items-center justify-center'>
-      <div className='md:w-1/3'>
+      <div className='md:w-1/2 md:pl-10'>
+        <p className='font-montserrat font-medium text-sm leading-5 opacity-40 tracking-wider mb-4 
+    md:text-lg md:leading-7'>
+          OUR MISSION
+        </p>
+        <h1 className='font-dmserif text-2xl font-normal leading-8 mb-6 md:text-4xl md:leading-10'> We are on a mission to <span className='text-spec-turquiose'>provide diverse peoples the resources and opportunities </span> essential to learn skills, build careers, and become leaders of sustainable social impact.
+        </h1>
+        <p className='font-montserrat opacity-70 font-medium text-base leading-normal md:text-lg md:leading-7'>
+          We  understand that for many people from underserved communities, they can&apos;t give back or help others until they are able to access essential goods and services, learn professional skills, and achieve physical and financial security. 
+        </p>
+      </div>
+      <div className='md:w-1/2'>
         <Image
           className="m-auto text-left mb-6 hidden md:block"
           src={missionImg}
@@ -21,18 +32,7 @@ export default function Mission() {
           height={100}
         />
       </div>
-      <div className='md:w-1/2 md:pl-10'>
-        <p className='font-montserrat font-medium text-sm leading-5 opacity-40 tracking-wider mb-4 
-    md:text-lg md:leading-7'>
-          OUR MISSION
-        </p>
-        <h1 className='font-dmserif text-2xl font-normal leading-8 mb-6 md:text-4xl md:leading-10'>Empower individuals to become globally-minded <span className='text-spec-turquiose'>change agents</span> by helping them learn the <span className='text-spec-turquiose'>essential skills</span> required to build sustainable careers.
-        </h1>
-        <p className='font-montserrat opacity-70 font-medium text-base leading-normal md:text-lg md:leading-7'>
-          We understand that for many people from underserved communities, they can&apos;t give back or help others until they are able to access essential goods and services,
-          learn professional skills, and achieve physical and financial security.
-        </p>
-      </div>
+      
     </div>
   )
 }
