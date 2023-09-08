3cc68cc941295dbc1f190f9255b518a819228fa0
Sam <stmiyamoto10@gmail.com>
2023-09-08 14:24:28 -0700

    Standardized class names, centered testimonials, fixed carousel indicators

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -40,63 +40,66 @@ export default function Testimonials() {
   cursor-pointer 
   border-2  
   border-solid 
-  border-black 
+  border-black
   rounded-full 
-  bg-white p-0 
+  bg-white 
+  p-0 
   -indent-[999px] 
+  opacity-50
   transition-opacity 
   duration-[600ms] 
   ease-[cubic-bezier(0.25,0.1,0.25,1.0)] 
   motion-reduce:transition-none
   `
+
   return (
-    
-    <div className="w-full m-auto p-10">
+    <div 
+    className="w-full m-auto p-10"
+    >
       <div
         id="carouselTestimonialsIndicators"
         className="relative"
         data-te-carousel-init
         data-te-carousel-slide
-      >
-        {/* <!--Carousel indicators--> */}
+        data-te-ride="carousel"
+        >
         {/* <!--Carousel items--> */}
-
-        <div
-          className="flex justify-evenly w-full md:h-104 overflow-hidden after:clear-both after:block after:content-['']">
+        <div 
+        className="flex justify-around ml-20 md:h-104 p-1.5 w-full overflow-hidden after:clear-both after:block after:content-['']"
+        >
           {/* <!--First item--> */}
           <div
-            className="flex md:-mr-[80%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+            className="flex justify-center md:-mr-[80%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
             data-te-carousel-item
             data-te-carousel-active
           >
             <div className={testimonialCardStyle}>
               <div className="flex justify-center">
-              <Image
-              className="z-10 flex justify-center -mt-14 rounded-full"
+                <Image
+                  // className="z-10 flex justify-center -mt-14 rounded-full"
                   src={"/Victoria.svg"}
                   alt="Victoria"
                   height={120}
                   width={120}
-              priority
-            />
-            </div>
-            <blockquote className="mb-8">
-                 <ReadMore description={testimonialsData[0].quote} limit={250}/>    
-            </blockquote> 
+                  priority
+                />
+              </div>
+              <blockquote className="mb-8">
+                  <ReadMore description={testimonialsData[0].quote} limit={250}/>    
+              </blockquote> 
               <div className=" flex-shrink border-t-2 pt-8 ml-20 mr-20 w-30 border-spec-sunshine "></div>
-              <p id="name" className="content-center text-xl font-medium tracking-widest font-poppins">VICTORIA LO</p>
+                <p id="name" className="content-center text-xl font-medium tracking-widest font-poppins">VICTORIA LO</p>
             </div>
           </div>
-
           {/* <!--Second item--> */}
           <div
-            className="relative md:-mr-[80%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+            className="flex justify-center md:-mr-[80%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
             data-te-carousel-item
           >
             <div className={testimonialCardStyle}>
             <div className="flex justify-center">
               <Image
-              className="z-10 w-28 h-28 flex justify-center -mt-14 rounded-full "
+              // className="z-10 w-28 h-28 flex justify-center -mt-14 rounded-full "
                   src={"/Ilia.svg"}
                   alt="Ilia"
                   height={150}
@@ -114,13 +117,13 @@ export default function Testimonials() {
           </div>
           {/* <!--Third item--> */}
           <div
-            className="md:-mr-[80%] hidden w-full md:h-104 transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+            className="flex justify-center md:-mr-[80%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
             data-te-carousel-item
           >
             <div className={testimonialCardStyle}>
              <div className="flex justify-center">
               <Image
-              className=" z-10 w-28 h-28 flex justify-center -mt-14 rounded-full"
+              // className=" z-10 w-28 h-28 flex justify-center -mt-14 rounded-full"
                   src={"/Yoline.svg"}
                   height={150}
                   width={150}
@@ -130,20 +133,20 @@ export default function Testimonials() {
             </div>
             <blockquote>
                 <ReadMore description={testimonialsData[2].quote} limit={250}/> 
-          </blockquote> 
+            </blockquote> 
               <div className=" flex-shrink border-t-2 ml-20 mr-20 w-30 border-spec-sunshine "></div>
               <p id="name" className="text-xl font-medium tracking-widest py-6">YOLINE BANERJEE</p>
             </div>
           </div>
              {/* <!--Fourth item--> */}
           <div
-            className="relative hidden float-left md:-mr-[80%] w-full md:h-104 transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+            className="flex justify-center md:-mr-[80%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none float-left "
             data-te-carousel-item
           >
             <div className={testimonialCardStyle}>
              <div className="flex justify-center">
               <Image
-              className="z-10 w-28 h-28 flex justify-center -mt-14 rounded-full"
+              // className="z-10 w-28 h-28 flex justify-center -mt-14 rounded-full"
               src={"/Alicia.svg"}
                   alt="Alicia"
                   width={150}
@@ -159,40 +162,39 @@ export default function Testimonials() {
             </div>
           </div>
         </div>
-
-    <div
-    className=" absolute left-0 right-0 z-2 h-12 flex list-none justify-center p-1.5"
-    data-te-carousel-indicators>
-    <button
-      type="button"
-      data-te-target="#carouselTestimonialsIndicators"
-      data-te-slide-to="0"
-      data-te-carousel-active
-      className={buttonClassStyle}
-      aria-current="true"
-      aria-label="Slide 1"></button>
-    <button
-      type="button"
-      data-te-target="#carouselTestimonialsIndicators"
-      data-te-slide-to="1"
-      className={buttonClassStyle}
-      aria-label="Slide 2"></button>
-    <button
-      type="button"
-      data-te-target="#carouselTestimonialsIndicators"
-      data-te-slide-to="2"
-      className={buttonClassStyle}
-      aria-label="Slide 3"></button>
-      <button
-      type="button"
-      data-te-target="#carouselTestimonialsIndicators"
-      data-te-slide-to="3"
-      className={buttonClassStyle}
-      aria-label="Slide 4">
-      </button>
-  </div>
-    </div>
+        {/* Carousel indicators */}
+        <div
+          className="flex justify-center md:h-104 p-1.5 absolute left-0 right-0 z-2 h-12 list-none"
+          data-te-carousel-indicators>
+          <button
+            type="button"
+            data-te-target="#carouselTestimonialsIndicators"
+            data-te-slide-to="0"
+            data-te-carousel-active
+            className={buttonClassStyle}
+            aria-current="true"
+            aria-label="Slide 1"></button>
+          <button
+            type="button"
+            data-te-target="#carouselTestimonialsIndicators"
+            data-te-slide-to="1"
+            className={buttonClassStyle}
+            aria-label="Slide 2"></button>
+          <button
+            type="button"
+            data-te-target="#carouselTestimonialsIndicators"
+            data-te-slide-to="2"
+            className={buttonClassStyle}
+            aria-label="Slide 3"></button>
+          <button
+            type="button"
+            data-te-target="#carouselTestimonialsIndicators"
+            data-te-slide-to="3"
+            className={buttonClassStyle}
+            aria-label="Slide 4"></button>
+        </div>
       </div>
+    </div>
     
   )
 }
