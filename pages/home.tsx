9b9253c445dc19cf7f558c71bdd561e1690e6421
Joe Torreggiani <joetorreggiani@gmail.com>
2023-10-02 22:51:46 -0400

    Update meta tags.

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,16 +1,16 @@
-import dynamic from 'next/dynamic';
-import Navbar from '../components/Navbar';
-import Hero from '../components/Hero';
-import MobileHero from '../components/MobileHero';
-import Footer from '../components/Footer';
-import ByTheNumbers from '../components/ByTheNumbers';
-import React from 'react';
-import ContactSection from '../components/Contact/ContactSection';
-import Mission from '../components/Mission';
-import Background from '../components/Background';
-import HowWeWork from '../components/HowWeWork';
-import WhatWeDo from '../components/WhatWeDo';
-import OurServices from '../components/OurServices';
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+import Navbar from '../components/Navbar'
+import MobileHero from '../components/MobileHero'
+import Footer from '../components/Footer'
+import ByTheNumbers from '../components/ByTheNumbers'
+import React from 'react'
+import ContactSection from '../components/Contact/ContactSection'
+import Mission from '../components/Mission'
+import Background from '../components/Background'
+import HowWeWork from '../components/HowWeWork'
+import WhatWeDo from '../components/WhatWeDo'
+import OurServices from '../components/OurServices'
 
 const GetInvolved = dynamic(() => import("../components/GetInvolved/GetInvolved"), {
    ssr: false,
@@ -20,9 +20,36 @@ const Testimonials = dynamic(() => import("../components/Testimonials"), {
    ssr: false,
 });
 
+export function AppHead() {
+  return (
+    <Head>
+      <title>
+        Sustainable Progress Equality Collective
+      </title>
+      <link rel="icon" type="image/png" href="/favicon-16x16.png" />
+      <meta property="og:title" content="Sustainable Progress and Equality Collective" />
+      <meta property="og:url" content="https://specollective.org/" />
+      <meta
+        property="og:image"
+        content="https://spec-bucket.nyc3.cdn.digitaloceanspaces.com/spec-logo-old.png"
+      />
+      <meta
+        property="og:description"
+        content="Helping people learn skills, build careers, and become leaders of sustainable social impact."
+      />
+      <meta
+        name="twitter:card"
+        content="https://spec-bucket.nyc3.cdn.digitaloceanspaces.com/spec-logo-old.png"
+      />
+      <meta name="twitter:title" content="Sustainable Progress and Equality Collective" />
+    </Head>
+  )
+}
+
 export default function Home() {
   return (
     <>
+      <AppHead />
       <Navbar />
       <MobileHero />
       <Mission />
