e5252cb0b7cc6403d92558729a5fd70da8fdc2a8
Sam <stmiyamoto10@gmail.com>
2023-10-03 10:44:01 -0700

    updating font to montserrat

--- components/DonationTable.tsx
+++ components/DonationTable.tsx
@@ -7,14 +7,14 @@ function DonationTable() {
         <tr>
         <th
           colSpan={2}
-          className="h-[2.5rem] bg-spec-banana border-4 border-spec-white rounded-tr-3xl rounded-tl-3xl text-xl py-2 text-center lg:px-4 xl:px-10 2xl:px-20"
+          className="h-[2.5rem] bg-spec-banana border-4 border-spec-white rounded-tr-3xl rounded-tl-3xl text-xl py-2 text-center lg:px-4 xl:px-10 2xl:px-20 font-montserrat"
           >
           <div>How your donation can make an <strong>impact</strong>!</div>
         </th>
         </tr>
       </thead>
       <tbody>
-        <tr className="bg-spec-lemon border-4 border-spec-white">
+        <tr className="bg-spec-lemon border-4 border-spec-white font-montserrat">
           <td className="px-2 py-2">
             $20
           </td>
@@ -22,7 +22,7 @@ function DonationTable() {
             Cover a learning resource
           </td>
         </tr>
-        <tr className="bg-spec-banana border-4 border-spec-white">
+        <tr className="bg-spec-banana border-4 border-spec-white font-montserrat">
           <td className="px-2 py-2">
             $500
           </td>
@@ -30,7 +30,7 @@ function DonationTable() {
             Career development stipend
           </td>
         </tr>
-        <tr className="bg-spec-lemon border-4 border-spec-white">
+        <tr className="bg-spec-lemon border-4 border-spec-white font-montserrat">
           <td className="px-2 py-2">
             $2000
           </td>
@@ -38,7 +38,7 @@ function DonationTable() {
             Fund a short fellowship
           </td>
         </tr>
-        <tr className="bg-spec-banana border-4 border-spec-white">
+        <tr className="bg-spec-banana border-4 border-spec-white font-montserrat">
           <td className="px-2 py-2">
             $5,000
           </td>
@@ -46,7 +46,7 @@ function DonationTable() {
             Create a community project
           </td>
         </tr>
-        <tr className="bg-spec-lemon border-4 border-spec-white">
+        <tr className="bg-spec-lemon border-4 border-spec-white font-montserrat">
           <td className="px-2 py-2 rounded-bl-3xl">
             $10,000
           </td>
