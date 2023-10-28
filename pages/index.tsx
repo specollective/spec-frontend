076f13adc3af473795a391fcf1551151589104fb
Joe Torreggiani <joetorreggiani@gmail.com>
2023-10-28 19:25:54 -0400

    Fix home data fetching.

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,7 +48,7 @@ export default function Home({ title, heroContent }: HomeProps) {
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID as string,
     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
